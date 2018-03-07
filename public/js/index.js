df97276b9cbd067a22a7b767597bf6661cc85501
delynko <deynko@gmail.com>
2018-03-07 15:50:29 -0700

    complete functionality to get restrooms within 1000 feet of a rout. add icons for facility type.

--- public/js/index.js
+++ public/js/index.js
@@ -4,35 +4,55 @@ var mapboxStreets = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y
     attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.satellite',
-    accessToken: 'pk.eyJ1IjoiZGVseW5rbyIsImEiOiJjaXBwZ3hkeTUwM3VuZmxuY2Z5MmFqdnU2In0.ac8kWI1ValjdZBhlpMln3w'
+    accessToken: 'pk.eyJ1IjoiZGVseW5rbyIsImEiOiJjaXBwZ3hkeTUwM3VuZmxuY2Z5MmFqdnU2In0.ac8kWI1ValjdZBhlpMln3w',
 });
 
 var map = L.map('map', {
     maxZoom: 18,
     layers: [mapboxStreets],
-}).setView([38.760, -95.874], 5);
+}).setView([37.229508312347555, -112.96417236328126], 14);
 map.zoomControl.setPosition('topright');
 
-var coordinates = '';
+var urlCoordinates = '';
 var x = 0;
 
 map.on('click', function(e){
     
+    var rteStop = L.marker([e.latlng.lat, e.latlng.lng]);
+    map.addLayer(rteStop);
+
     var lat = e.latlng.lat.toString();
     var lng = e.latlng.lng.toString();
-    
+ 
     if (x == 0) {
-        L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-        coordinates += `${lng},${lat}`;
+        urlCoordinates += `${lng},${lat}`;
     } else {
-        L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-        coordinates += `;${lng},${lat}`;
+        urlCoordinates += `;${lng},${lat}`;
     };
     x += 1;
+
+    
+
 });
 
 $('#submit').on('click', function(){
-    socket.emit('get-coordinates', coordinates, function(callback){
-        L.polyline(callback, {color: 'red'}).addTo(map);
+    map.removeEventListener('click');
+    socket.emit('get-url-coordinates', urlCoordinates, function(callback){
+        var route = L.polyline(callback, {color: 'red'});
+        map.addLayer(route);
     });
+});
+
+socket.on('restWat-points', function(points){
+    L.geoJSON(points, {
+        onEachFeature: function(feature, layer){
+            if (layer.feature.properties.TOILET_TYP == 'F'){
+                layer.setIcon(new L.icon({iconUrl: "images/rest-water.png", iconSize: [20, 20]}));
+            } else if (layer.feature.properties.TOILET_TYP == 'V'){
+                layer.setIcon(new L.icon({iconUrl: "images/toilet.png", iconSize: [20, 20]}));
+            } else {
+                layer.setIcon(new L.icon({iconUrl: "images/water.png", iconSize: [20, 20]}));
+            }
+        }
+    }).addTo(map);
 })
--- server/server.js
+++ server/server.js
@@ -7,11 +7,18 @@ const socketIO = require('socket.io');
 const hbs = require('hbs');
 const curl = require('curl');
 const polyline = require('polyline');
+const turf = require('@turf/turf');
+const helper = require('@turf/helpers');
 
 const port = process.env.PORT || 3000;
 
 const datasetId = process.env.DATASET_ID;
 const client = new MapboxClient(process.env.MAPBOX_PRIVATE);
+
+var restWat;
+client.listFeatures(datasetId, {}, (err, collection) => {
+    restWat = collection;
+});
 
 var app = express();
 var server = http.createServer(app);
@@ -32,17 +39,42 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
     console.log('user connected');
 
-    socket.on('get-coordinates', (data, callback) => {
+    socket.on('get-url-coordinates', (urlCoordinates, callback) => {
 
-        curl.get(`https://api.mapbox.com/directions/v5/mapbox/cycling/${data}?access_token=${process.env.MAPBOX_PUBLIC}`, {
+        curl.get(`https://api.mapbox.com/directions/v5/mapbox/cycling/${urlCoordinates}?access_token=${process.env.MAPBOX_PUBLIC}`, {
             roundtrip: false
         }, (err, response, body) => {
             var bodyParse = JSON.parse(body);
 
             var line = polyline.decode(bodyParse.routes[0].geometry);
+
+            var bufferCoordinates = (lineCoords) => {
+                var flipCoords = [];
+
+                for (i = 0; i < lineCoords.length; i++) {
+
+                    flipCoords.push([(lineCoords[i][1]), lineCoords[i][0]]);
+                }
+                return flipCoords
+            };
+
+            var lineForBuffer = helper.lineString(bufferCoordinates(line));
+
+            var buffer = turf.buffer(lineForBuffer, 1000, {
+                units: 'feet'
+            });
+
+            var pointsInBuffer = (buff) => {
+                var pointsInBuffer = turf.pointsWithinPolygon(restWat, buff);
+                socket.emit('restWat-points', pointsInBuffer);      
+            };
+
+            pointsInBuffer(buffer);
+
             callback(line);
         });
     });
+
 
     socket.on('disconnect', () => {
         console.log('user disconnected');
